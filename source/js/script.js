f031ef6acf2d51fb4fe40341326a691913208676
Валерия Тросянова <111880353+leronana@users.noreply.github.com>
2023-07-02 03:39:33 +0400

    2 commit

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,62 @@
-/* в этот файл добавляет скрипты*/
+/* Menu */
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+/* Swiper */
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+/* Map */
+
+const position = [59.968266, 30.3174089];
+const map = L.map('map').setView(position, 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/svg/map-pin.svg',
+  iconSize: [39, 50],
+  iconAnchor: [18, 50],
+});
+
+const mainPin = L.marker(
+  {
+    lat: position[0],
+    lng: position[1],
+  },
+  {
+    draggable: false,
+    icon: mainPinIcon,
+  },
+);
+mainPin.addTo(map);
+
+
