625bb4d416e265e49182d692814973f38be5cd04
Валерия Тросянова <111880353+leronana@users.noreply.github.com>
2023-07-02 13:43:41 +0400

    Последний коммит

--- source/js/script.js
+++ source/js/script.js
@@ -1,48 +1,46 @@
 /* Menu */
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+let navMain = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
 });
 
 /* Swiper */
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   loop: true,
 
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-
 });
 
 /* Map */
 
 const position = [59.968266, 30.3174089];
-const map = L.map('map').setView(position, 17);
+const map = L.map("map").setView(position, 17);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-
 const mainPinIcon = L.icon({
-  iconUrl: './img/svg/map-pin.svg',
+  iconUrl: "./img/svg/map-pin.svg",
   iconSize: [39, 50],
   iconAnchor: [18, 50],
 });
@@ -55,8 +53,6 @@ const mainPin = L.marker(
   {
     draggable: false,
     icon: mainPinIcon,
-  },
+  }
 );
 mainPin.addTo(map);
-
-
